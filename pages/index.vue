b83800e91777fa7165d59b630e733c866f0c4c4d
Lumer Montepio <lumermontepio@gmail.com>
2022-08-08 00:03:53 +0800

    add styles

--- pages/index.vue
+++ pages/index.vue
@@ -9,12 +9,33 @@
         v-model="newTask" 
         placeholder="Add a new task" 
         @keypress.enter="addTask" />
+      <button @click="addTask">Add</button>
     </div>
+
+    <div class="tasks">
+      <Task 
+        v-for="(task, i) in $store.state.tasks"
+        :key="i"
+        :task="task" />
+    </div>
+    
   </main>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  data () {
+    return {
+      newTask: ''
+    }
+  },
+  methods: {
+    addTask () {
+      if (this.newTask) {
+        this.$store.commit('ADD_TASK', this.newTask);
+        this.newTask = '';
+      }
+    }
+  }
 }
 </script>
